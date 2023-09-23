1b7afb378970f223aeaee7bef457c5791648d827
luciD <sosalnet@yandex.ru>
2023-09-23 20:44:30 +0300

    changed logic in like

--- src/components/Like/Like.tsx
+++ src/components/Like/Like.tsx
@@ -10,16 +10,20 @@ export const Like = ({ data }: { data: any }) => {
   const dispatch = useAppDispatch();
   const location = useLocation();
   const id = location.pathname === '/' ? data.id : data.itemId;
+  const title = data.title;
+
+  const cardName = data.title; 
 
   const favoriteCardsJson = localStorage.getItem('favoriteCards');
-  const favoriteCards = favoriteCardsJson ? JSON.parse(favoriteCardsJson) : [];
+  const favoriteCardsNamesJson = localStorage.getItem('favoriteCardsNames');
 
-  const isFavorite = favoriteCards.includes(id);
+  const favoriteCards = favoriteCardsJson ? JSON.parse(favoriteCardsJson) : [];
+  const favoriteCardsNames = favoriteCardsNamesJson ? JSON.parse(favoriteCardsNamesJson) : [];
+
+  const isFavorite = favoriteCardsNames.includes(title);
 
   const handleClickFavorite = () => {
     const newIsFavorite = !isFavorite;
-
-    dispatch(updateFavorite({ favorite: newIsFavorite, id }));
 
     if (location.pathname === '/') {
       dispatch(updateFavorite({ favorite: newIsFavorite, id }));
@@ -29,11 +33,19 @@ export const Like = ({ data }: { data: any }) => {
 
     if (newIsFavorite) {
       favoriteCards.push(id);
+      favoriteCardsNames.push(cardName);
     } else {
       favoriteCards.splice(favoriteCards.indexOf(id), 1);
+      const indexToRemove = favoriteCards.indexOf(id);
+            if (indexToRemove !== -1) {
+              favoriteCards.splice(indexToRemove, 1);
+              favoriteCardsNames.splice(indexToRemove, 1);
+            }
     }
 
     localStorage.setItem('favoriteCards', JSON.stringify(favoriteCards));
+    localStorage.setItem('favoriteCardsNames', JSON.stringify(favoriteCardsNames)); 
+
   };
 
   const typesImg = isFavorite ? heart_clicked : heart;
